ec753204af78c2c498a81e6b2b970d9aa7b02d19
Lucas Cotner <lc07@uw.edu>
2014-08-26 11:44:50 -0700

    Returning values. Also including node Mods to remember to use the Q library

--- Promises/exerciseFive.js
+++ Promises/exerciseFive.js
@@ -1,7 +1,23 @@
 var q = require('q');
 var defer = q.defer();
 
-defer.promise.then(console.log);
+//defer.promise.then(attachTitle);
 
-defer.resolve("SECOND");
-console.log("FIRST");
+
+// console.log("FIRST");
+
+function attachTitle (name) {
+	return "DR. " + name;
+};
+
+
+//defer.promise.then(attachTitle);
+//defer.promise.then(console.log);
+defer.promise
+  .then(attachTitle)
+  .then(console.log);
+
+defer.resolve("MANHATTAN");
+
+
+
